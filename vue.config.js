c08b373b04b014e2511e1230dd4df91f99e0e664
av3r6un <av3rgun@yandex.com>
2024-07-07 00:42:38 +0300

    Fixed builds for github p

--- vue.config.js
+++ vue.config.js
@@ -2,4 +2,7 @@ const { defineConfig } = require('@vue/cli-service');
 
 module.exports = defineConfig({
   transpileDependencies: true,
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/stolpotvorenie.space'
+    : '/' // only for GitHub Pages
 });
