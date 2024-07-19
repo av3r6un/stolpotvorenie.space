9060e087cd1999d8a4f0254f456861c88c247824
av3r6un <av3rgun@yandex.com>
2024-07-19 16:16:04 +0300

    Modified for deleting

--- vue.config.js
+++ vue.config.js
@@ -2,6 +2,7 @@ const { defineConfig } = require('@vue/cli-service');
 
 module.exports = defineConfig({
   transpileDependencies: true,
+  outputDir: 'frontend/',
   pwa: {
     workboxOptions: {
       navigateFallback: 'index.html',
